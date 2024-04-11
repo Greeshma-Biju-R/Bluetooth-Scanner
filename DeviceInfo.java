72a595055e86bcc0df1c1a27e3f1708d10af19bd
Greeshma Biju R <141510907+Greeshma-Biju-R@users.noreply.github.com>
2024-04-11 11:58:37 +0530

    Create DeviceInfo.java

---
+++ DeviceInfo.java
@@ -0,0 +1,21 @@
+package com.example.bluetoothscanner;
+
+public class DeviceInfo {
+    private int rssi;
+    private String address;
+    private long timestamp;
+
+    public DeviceInfo(int rssi, String address, long timestamp) {
+        this.rssi = rssi;
+        this.address = address;
+        this.timestamp = timestamp;
+    }
+
+    public String getRssi() {
+        return String.valueOf(this.rssi);
+    }
+}
+
+
+
+
